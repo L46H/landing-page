94bed6b90faeb3e41503a011520f0618dd89d66e
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-02-13 08:00:10 +0100

    getting location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,9 @@ import { AppComponent } from './app.component';
 import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    WeatherModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WeatherModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForecastService {
+  constructor() {}
 
-  constructor() { }
+  getCurrentLocation() {
+    return new Observable<Coordinates>(observer => {
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+          observer.next(position.coords);
+          observer.complete();
+        },
+        err => observer.error(err)
+      );
+    });
+  }
 }
