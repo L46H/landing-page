85f808e48f002f0a643b27578be216eace3587f0
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-02-13 20:42:01 +0100

    getting forecast

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface Coordinates {
+  accuracy: number;
+  altitude: number | null;
+  altitudeAccuracy: number | null;
+  heading: number | null;
   latitude: number;
   longitude: number;
+  speed: number | null;
 }
 
 @Injectable({
@@ -11,6 +18,18 @@ interface Coordinates {
 })
 export class ForecastService {
   constructor() {}
+
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map(coords => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', 'e4443779dce4cf41ee40eb0b3504a514');
+      })
+    );
+  }
 
   getCurrentLocation() {
     return new Observable<Coordinates>(observer => {
