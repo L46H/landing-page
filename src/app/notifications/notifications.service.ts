3715ec619ec34b270c97af6e73d294365a784ae8
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-02-18 15:01:24 +0100

    implemented success, error, clear functions

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  // optional
+  text?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationsService {
+  messages: Subject<Command>;
 
-  constructor() { }
+  constructor() {
+    // subject is listening before we call below methods
+    this.messages = new Subject<Command>();
+  }
+
+  addSuccess(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success',
+    });
+  }
+
+  addError(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error',
+    });
+  }
+
+  clearMessage(id: number) {
+    this.messages.next({
+      // id: id
+      id,
+      type: 'clear',
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
