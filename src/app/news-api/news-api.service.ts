d0f9026502fe4b3127debafcb95f857a5fd05667
Hubert Kieruczenko <hub3rt65@gmail.com>
2023-03-31 09:40:51 +0200

    switch map and tap inside news-api.service

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { tap, map, switchMap } from 'rxjs/operators';
-import { HttpParams } from '@angular/common/http';
+import { HttpParams, HttpClient } from '@angular/common/http';
+
+interface NewsApiResponse {
+  totalResults: number;
+  articles: {
+    title: string;
+    url: string;
+  }[]
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +22,11 @@ export class NewsApiService {
 
   pagesInput: Subject<number>;
   pagesOutput: Observable<any>;
-  numberOfPages: Observable<number>;
+  numberOfPages: Subject<number>;
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.numberOfPages = new Subject<number>();
+
     this.pagesInput = new Subject<number>();
     this.pagesOutput = this.pagesInput.pipe(
       map(page => {
@@ -25,6 +35,12 @@ export class NewsApiService {
           .set('country', this.country)
           .set('pagesSize', this.pageSize)
           .set('page', page);
+      }),
+      switchMap(params => this.http.get<NewsApiResponse>(this.url, { params: params })),
+      tap(response => {
+        // 55 / 10 = 5.5 -> 6 pages
+        const totalPages = Math.ceil(response.totalResults / this.pageSize);
+        this.numberOfPages.next(totalPages);
       })
     )
   }
